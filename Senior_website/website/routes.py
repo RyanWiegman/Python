66990c7a0bed89ab432e2819b18a48365148751f
RyanWiegman <ryanwiegman@gmail.com>
2022-07-20 17:12:14 -0400

    adding all documentation

--- Senior_website/website/routes.py
+++ Senior_website/website/routes.py
@@ -9,3 +9,27 @@ def home():
 @app.route("/about")
 def about() :
     return render_template('about.html', title='About')
+
+@app.route("/Initial_Block_Document")
+def Initial_Block_Document() :
+    return render_template("initial_and_block.html", title="Initial and Block Document")
+
+@app.route("/senior_design")
+def senior_design() :
+    return render_template('senior_design.html', title="Senior Design")
+
+@app.route('/CDR')
+def CDR() :
+    return render_template("cdr.html", title="Critial Design Review")
+
+@app.route('/conference_paper')
+def conference_paper() :
+    return render_template('conference_paper.html', title="Conference Paper")
+
+@app.route('/final_presentation')
+def final_presentation() :
+    return render_template("final_presentation.html", title="Final Presentation")
+
+@app.route('/final_documentation')
+def final_documentation() :
+    return render_template("final_documenation.html", title='Final Documentation')
